9f43cffc4d83bb172e329b2bcf9e27bcae118108
AlexandrIgnat <ignatyevaleksandr.1992@mail.ru>
2023-05-21 22:27:36 +0300

    added point marker to map

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 new WOW().init();
 
-
 // map 
 
 ymaps.ready(init);
+
 function init() {
     var myMap = new ymaps.Map("map", {
         center: [59.94366822276193,30.320574030339184],
@@ -12,12 +12,11 @@ function init() {
 
     myMap.controls.remove('default');
 
-    var myGeoObject = new ymaps.GeoObject({
-        geometry: {
-            type: "Point", // тип геометрии - точка
-            coordinates: [59.94366822276193,30.320574030339184] // координаты точки
-        }
-    });
+    var myPlacemark = new ymaps.Placemark([59.94366822276193,30.320574030339184], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'image/location.svg',
+        iconImageSize: [20, 56],
+      });
     
-    myMap.geoObjects.add(myGeoObject); 
+    myMap.geoObjects.add(myPlacemark); 
 }
