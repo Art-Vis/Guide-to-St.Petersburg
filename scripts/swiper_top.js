7c23e79a1c2a6cde7294c259c2264e7cabb465c6
AlexandrIgnat <ignatyevaleksandr.1992@mail.ru>
2023-05-25 22:49:27 +0300

    add swiper pagination and change background image in info__sub block

--- scripts/swiper_top.js
+++ scripts/swiper_top.js
@@ -1,7 +1,10 @@
 const projectsPartnerSwiper = new Swiper('.slider', {
     slidesPerView: 1,
     slidesPerGroup: 1,
-  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
